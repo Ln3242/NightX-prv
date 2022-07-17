c32267017205d83a2076b73272697e1de4f5fa06
Ln3242 <109390328+Ln3242@users.noreply.github.com>
2022-07-17 15:19:21 -0500

    Create NewMainScript.lua

---
+++ NewMainScript.lua
@@ -0,0 +1,3 @@
+if shared.LoadPrivate then shared.NightXPrivate, shared.SECRETKEY = true, "NightXPrivate_PJK78TSG1HQUGRKVIMPA" end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ln3242/NightX-prv/main/bedwars.lua", true))() -- Fixed private haha, engo is trash.
